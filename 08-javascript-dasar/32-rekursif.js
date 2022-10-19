609c44fe382cf55e677772e30567adb7f6153d22
khafidhkayyis <khafidhkayyis@gmail.com>
2022-10-19 20:23:30 +0700

    apaituscss

---
+++ 08-javascript-dasar/32-rekursif.js
@@ -0,0 +1,22 @@
+// Rekursif = sebuah fungsi yang memanggil dirinya sendiri
+
+// sebelumnya kita mengingat kembali karakteristik penggunaan function
+
+// Input => Function => output
+
+// contoh gambaran rekursif bisa lihat pada gambar
+
+// ketika menggunakan rekursif pastikan harus berakhir dan menghasilkan
+// nilai
+
+// contoh kode sederhana
+
+// function tes(){
+//     return tes();
+// }
+
+// tes();
+
+// jika kita mempunyain function tes dan didalamnya mengembalikan nilai tes itu sendiri
+// ketika dijalankan maka function tes akan memanggil dirinya sendiri secara terus menerus
+// dan akhirnya memunculkan tulisan di console "maximum call stack size exceeded"
